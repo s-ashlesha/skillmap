912c43945eb087205ea2fc1bf2f4f21a41c17846
s-ashlesha <ashleshasarma4206@gmail.com>
2026-03-21 17:18:13 +0530

    api url fixed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // Added for Docker compatibility
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://skillmap-backend-wigj.onrender.com/api",
   },
 }
 
